511689197889166f6829ece44e938d3f4335b273
Dougie Zoraida <dzoraida@se-h6.nsk.21-school.ru>
2021-12-27 23:58:54 +0700

    develop

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -13,22 +13,88 @@ typedef struct {
     value_type_t value_type;
 } s21_decimal;
 
+typedef union {
+  unsigned int integerValue;
+  float floatValue;
+} value;
+
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
+int s21_from_float_to_decimal(float src, s21_decimal *dst);
 
 int set_bit(int position, s21_decimal *dst);
 int init_decimal(s21_decimal *dst);
 int check_bit(int position, s21_decimal dst);
+int get_binary_power(value val);
+int form_float_decimal(s21_decimal *dst, int binaryPower, int tenPower, value val);
 
 int main() {
-    s21_decimal decimal;
-    init_decimal(&decimal);
-    int start = -24;
-    int result = 0;
-    s21_from_int_to_decimal(start, &decimal);
-    s21_from_decimal_to_int(decimal, &result);
-    printf("%d\n%d\n", start, result);
+float a = 1E24;
+s21_decimal decimal;
+init_decimal(&decimal);
+s21_from_float_to_decimal(a, &decimal);
+for (int i = 95; i >= 0; i--) {
+    if (check_bit(i, decimal))
+    printf("1");
+    else 
+    printf("0");
+}
+
+// value val;
+// val.floatValue = (int)a;
+// for (int i = 31; i >= 0; i--) {
+//     if (val.integerValue & (1 << i))
+//     printf("1");
+//     else 
+//     printf("0");
+// }
+
+// printf("\n%d\n", get_binary_power(val));
+// for (int i = 31; i >= 0; i--) {
+//     if ((a & (1 << i)) == 0)
+//         printf("0");
+//     else 
+//         printf("1");
+// }
+    // s21_decimal decimal;
+    // init_decimal(&decimal);
+    // int start = -24;
+    // int result = 0;
+    // s21_from_int_to_decimal(start, &decimal);
+    // s21_from_decimal_to_int(decimal, &result);
+    // printf("%d\n%d\n", start, result);
     return 0;
+}
+
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+    int result = 1;
+    int tenPower = 0;
+    value val;
+    int binaryPower;
+    double number = (double) src;
+    if (number != 0.0 && dst) {
+        for (; !(int)(number / 1E7); number *= 10)
+            tenPower += 1;
+        val.floatValue = number;
+        binaryPower = get_binary_power(val);
+        printf("binary:%d\n", binaryPower);
+        form_float_decimal(dst, binaryPower, tenPower, val);
+    }
+    return result;
+}
+
+
+int form_float_decimal(s21_decimal *dst, int binaryPower, int tenPower, value val) {
+    if (val.integerValue & (1 << 31))
+        set_bit(127, dst);
+    set_bit(binaryPower, dst);
+    int j = binaryPower;
+    for (int i = 22; i >= 0; i--) {
+        j--;
+        if (val.integerValue & (1 << i))
+            set_bit(j, dst);
+    }
+    return 1;
 }
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
@@ -75,4 +141,17 @@ int init_decimal(s21_decimal *dst) {
     for (int i = 0; i < 4; i++)
         dst->bits[i] = 0;
     return 1;
+}
+
+int get_binary_power(value val) {
+    int result = 0, mult = 0;
+    for (int i = 30; i > 22; i--){
+        if ((val.integerValue & (1 << i)) == 0)
+            mult = 0;
+        else 
+            mult = 1;
+    result += mult * pow(2, i - 23);
+    }
+    result -= 127;
+    return result;
 }
